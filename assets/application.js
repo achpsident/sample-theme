fa93378a71ab7b2555cabbc540e8ae88aa769deb
Matthew Stuewe <stuewe.matthew@gmail.com>
2019-07-24 14:37:41 +0900

    test

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,16 @@
 // Put your applicaiton javascript here
+
+
+// const addToCart = document.querySelector("#AddToCart");
+//   if (addToCart) {
+//   console.log("found");
+//   addToCart.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     console.log("test");
+
+
+
+// });
+
+// }
+
