69504341a6d0ad46bff8f583656b3de058fa2e30
Matthew Stuewe <stuewe.matthew@gmail.com>
2019-07-24 19:20:04 +0900

    final commit

--- assets/application.js
+++ assets/application.js
@@ -1,19 +1,3 @@
-// Put your applicaiton javascript here
-
-
-// const addToCart = document.querySelector("#AddToCart");
-//   if (addToCart) {
-//   console.log("found");
-//   addToCart.addEventListener("submit", (event) => {
-//     event.preventDefault();
-//     console.log("test");
-
-
-
-// });
-
-// }
-
 async function addItem(variantId, quantity) {
     console.log("test")
     const result = await fetch("/cart/add.json", {
