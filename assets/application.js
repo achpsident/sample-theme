19bddffc5f9096efe6acf9963164ca3593bce28d
Matthew Stuewe <stuewe.matthew@gmail.com>
2019-07-24 18:53:33 +0900

    almost done

--- assets/application.js
+++ assets/application.js
@@ -14,3 +14,17 @@
 
 // }
 
+async function addItem(variantId, quantity) {
+    console.log("test")
+    const result = await fetch("/cart/add.json", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            id: variantId,
+            quantity: quantity
+        })
+    })
+}
